7058ac260be68b5e79da136f4a6de5d05a340200
rizkiaisyahyunita <yrizkiaisyah@gmail.com>
2026-02-20 14:52:09 +0700

    commit kedua

--- latihan_2407089/src/latihan_pbo/latihan1_2407089/Main.java
+++ latihan_2407089/src/latihan_pbo/latihan1_2407089/Main.java
@@ -1,7 +1,34 @@
 package latihan_pbo.latihan1_2407089;
 
+class Cafe{
+    String nama;
+    String alamat;
+    String menu;
+
+    //contrukstor
+    Cafe(String nama, String alamat, String menu) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.menu = menu;
+        System.out.println("Nama Cafe  : " + nama);
+        System.out.println("Alamat     : " + alamat);
+        System.out.println("Menu       : " + menu);
+        System.out.println();
+    }
+
+    //method
+    
+    void buka(){
+        System.out.println("Cafe sudah buka");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Praktikum 1");
+
+        Cafe cafe1 = new Cafe("Halaman", "Indramayu", "Mie Goreng");
+        cafe1.buka();
+        Cafe cafe2 = new Cafe("Ranum", "Indramayu Jabar", "Roti Bakar");
+        cafe2.buka();
     }
 }
